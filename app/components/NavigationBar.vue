9686331180878ed2420e2fcb0cca102b6fed8096
Robinson Lam <robinsonlam94@gmail.com>
2025-08-07 23:26:35 +1000

    cleanup navigation component

--- app/components/NavigationBar.vue
+++ app/components/NavigationBar.vue
@@ -1,55 +1,25 @@
 <template>
-  <nav class="navigation-bar">
-    <div class="nav-container">
+  <nav class="font-sans">
+    <div class="flex justify-between items-center px-16 py-12">
       <!-- Logo/Brand -->
-      <NavLink to="/" :hide-active="true"> littlerob. </NavLink>
+      <NavLink to="/" :hide-active="true">littlerob.</NavLink>
 
       <!-- Navigation Links -->
-      <div class="nav-links">
+      <div class="flex gap-8 ml-auto">
         <NavLink to="/">home</NavLink>
-        <NavLink to="/projects"> projects </NavLink>
-        <NavLink to="/career"> career </NavLink>
-        <NavLink to="/skills"> skills </NavLink>
+        <NavLink to="/projects">projects</NavLink>
+        <NavLink to="/career">career</NavLink>
+        <NavLink to="/skills">skills</NavLink>
       </div>
 
-      <span
-        class="w-[2px] h-[1rem] mx-[2rem] bg-white hover:bg-yellow-500"
-      ></span>
+      <NavigationDivider />
 
       <!-- Social Links -->
-      <div class="nav-social">
-        <NavLink href="https://github.com/robinsonlam" target="_blank">
-          github.
-        </NavLink>
-        <NavLink href="https://linkedin.com/in/robinsonlam" target="_blank">
-          linkedin.
-        </NavLink>
-        <NavLink href="https://bsky.app" target="_blank"> bluesky. </NavLink>
+      <div class="flex gap-8">
+        <NavLink href="https://github.com/robinsonlam" target="_blank">github.</NavLink>
+        <NavLink href="https://linkedin.com/in/robinsonlam" target="_blank">linkedin.</NavLink>
+        <NavLink href="https://bsky.app" target="_blank">bluesky.</NavLink>
       </div>
     </div>
   </nav>
 </template>
-
-<style scoped>
-.navigation-bar {
-  font-family: "Pixelify Sans", monospace;
-}
-
-.nav-container {
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  padding: 3rem 4rem;
-}
-
-.nav-links {
-  display: flex;
-  gap: 2rem;
-  margin-left: auto;
-}
-
-.nav-social {
-  display: flex;
-  gap: 2rem;
-}
-</style>
---
+++ app/components/NavigationDivider.vue
@@ -0,0 +1,5 @@
+<template>
+  <span
+    class="w-[2px] h-[1rem] mx-[2rem] bg-white hover:bg-yellow-500"
+  ></span>
+</template>
